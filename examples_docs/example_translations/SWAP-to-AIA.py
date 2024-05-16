c267555c73528143afbbf5520aee13ad748e9523
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-05-16 16:25:37 +0200

    simplify for test

--- examples_docs/example_translations/SWAP-to-AIA.py
+++ examples_docs/example_translations/SWAP-to-AIA.py
@@ -1,1 +1,9 @@
+"""
+==========================
+Translation of SWAP to AIA
+==========================
+This example shows how to enhance `PROBA2/SWAP <https://proba2.sidc.be/about/SWAP>`__ observations to `SDO/AIA <https://sdo.gsfc.nasa.gov/mission/instruments.php>'__ observations.
+"""
+
+
 print('SWAP-to-AIA.py: Done')
