a1461abd3d351b4959cd8117fc92b75ef916a525
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-05-17 11:16:34 +0200

    formatting text

--- iti/evaluation/evaluation.py
+++ iti/evaluation/evaluation.py
@@ -123,60 +123,60 @@ def saveToFITS(maps, path):
 
 ################################### Download GCP bucket ###################################
 
-def download_bucket_with_transfer_manager(
-        bucket_name, destination_directory="", workers=8, max_results=1000):
-        """Download all of the blobs in a bucket, concurrently in a process pool.
+def download_bucket_with_transfer_manager(bucket_name, destination_directory="", workers=8, max_results=1000):
 
-        The filename of each blob once downloaded is derived from the blob name and
-        the `destination_directory `parameter. For complete control of the filename
-        of each blob, use transfer_manager.download_many() instead.
+    """Download all of the blobs in a bucket, concurrently in a process pool.
 
-        Directories will be created automatically as needed, for instance to
-        accommodate blob names that include slashes.
-        """
+    The filename of each blob once downloaded is derived from the blob name and
+    the `destination_directory `parameter. For complete control of the filename
+    of each blob, use transfer_manager.download_many() instead.
 
-        # The ID of your GCS bucket
-        # bucket_name = "your-bucket-name"
+    Directories will be created automatically as needed, for instance to
+    accommodate blob names that include slashes.
+    """
 
-        # The directory on your computer to which to download all of the files. This
-        # string is prepended (with os.path.join()) to the name of each blob to form
-        # the full path. Relative paths and absolute paths are both accepted. An
-        # empty string means "the current working directory". Note that this
-        # parameter allows accepts directory traversal ("../" etc.) and is not
-        # intended for unsanitized end user input.
-        # destination_directory = ""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
 
-        # The maximum number of processes to use for the operation. The performance
-        # impact of this value depends on the use case, but smaller files usually
-        # benefit from a higher number of processes. Each additional process occupies
-        # some CPU and memory resources until finished. Threads can be used instead
-        # of processes by passing `worker_type=transfer_manager.THREAD`.
-        # workers=8
+    # The directory on your computer to which to download all of the files. This
+    # string is prepended (with os.path.join()) to the name of each blob to form
+    # the full path. Relative paths and absolute paths are both accepted. An
+    # empty string means "the current working directory". Note that this
+    # parameter allows accepts directory traversal ("../" etc.) and is not
+    # intended for unsanitized end user input.
+    # destination_directory = ""
 
-        # The maximum number of results to fetch from bucket.list_blobs(). This
-        # sample code fetches all of the blobs up to max_results and queues them all
-        # for download at once. Though they will still be executed in batches up to
-        # the processes limit, queueing them all at once can be taxing on system
-        # memory if buckets are very large. Adjust max_results as needed for your
-        # system environment, or set it to None if you are sure the bucket is not
-        # too large to hold in memory easily.
-        # max_results=1000
+    # The maximum number of processes to use for the operation. The performance
+    # impact of this value depends on the use case, but smaller files usually
+    # benefit from a higher number of processes. Each additional process occupies
+    # some CPU and memory resources until finished. Threads can be used instead
+    # of processes by passing `worker_type=transfer_manager.THREAD`.
+    # workers=8
 
-        storage_client = Client()
-        bucket = storage_client.bucket(bucket_name)
-        os.makedirs(destination_directory, exist_ok=True)
+    # The maximum number of results to fetch from bucket.list_blobs(). This
+    # sample code fetches all of the blobs up to max_results and queues them all
+    # for download at once. Though they will still be executed in batches up to
+    # the processes limit, queueing them all at once can be taxing on system
+    # memory if buckets are very large. Adjust max_results as needed for your
+    # system environment, or set it to None if you are sure the bucket is not
+    # too large to hold in memory easily.
+    # max_results=1000
 
-        blob_names = [blob.name for blob in bucket.list_blobs(max_results=max_results)]
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+    os.makedirs(destination_directory, exist_ok=True)
 
-        results = transfer_manager.download_many_to_path(
-            bucket, blob_names, destination_directory=destination_directory, max_workers=workers
-        )
+    blob_names = [blob.name for blob in bucket.list_blobs(max_results=max_results)]
 
-        for name, result in zip(blob_names, results):
-            # The results list is either `None` or an exception for each blob in
-            # the input list, in order.
+    results = transfer_manager.download_many_to_path(
+        bucket, blob_names, destination_directory=destination_directory, max_workers=workers
+    )
 
-            if isinstance(result, Exception):
-                print("Failed to download {} due to exception: {}".format(name, result))
-            else:
-                print("Downloaded {} to {}.".format(name, destination_directory + name))
+    for name, result in zip(blob_names, results):
+        # The results list is either `None` or an exception for each blob in
+        # the input list, in order.
+
+        if isinstance(result, Exception):
+            print("Failed to download {} due to exception: {}".format(name, result))
+        else:
+            print("Downloaded {} to {}.".format(name, destination_directory + name))
