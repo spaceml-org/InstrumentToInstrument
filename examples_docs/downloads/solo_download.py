a582aeb47ceaef7481fe977f4bfa4e396d3eccf9
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-05-18 19:43:38 +0200

    formatting text

---
+++ examples_docs/downloads/solo_download.py
@@ -0,0 +1,6 @@
+"""
+=========================
+Download PROBA2/SWAP data
+=========================
+This example shows how to download PROBA2/SWAP data from the `PROBA2 Science center <https://proba2.sidc.be/swap/data/bsd/>`__ using the ITI tool.
+"""
