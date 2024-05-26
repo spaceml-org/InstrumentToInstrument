fe965b2f3e85b71114063e6074fe8d8162444c55
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-05-26 13:02:27 +0200

    debug training

--- examples_docs/training/swap_to_aia.py
+++ examples_docs/training/swap_to_aia.py
@@ -46,23 +46,26 @@ with open('iti-testset/configs/proba2_to_sdo_colab.yaml', "r") as stream:
         print(exc)
 
 print(config)
-#base_dir = config['base_dir']
-#os.makedirs(base_dir, exist_ok=True)
+base_dir = config['base_dir']
+os.makedirs(base_dir, exist_ok=True)
 
 ########################################################################################
 # Initialize the paths from the config file
-#data_config = config['data']
-#swap_path = data_config['A_path']
-#swap_converted_path = data_config['converted_A_path']
-#os.makedirs(swap_converted_path, exist_ok=True)
-#sdo_path = data_config['B_path']
-#sdo_converted_path = data_config['converted_B_path']
-#os.makedirs(sdo_converted_path, exist_ok=True)
+data_config = config['data']
+swap_path = data_config['A_path']
+swap_converted_path = data_config['converted_A_path']
+os.makedirs(swap_converted_path, exist_ok=True)
+sdo_path = data_config['B_path']
+sdo_converted_path = data_config['converted_B_path']
+os.makedirs(sdo_converted_path, exist_ok=True)
 
 ########################################################################################
 # Define the training and validation months
-#test_months = [11, 12]
-#train_months = list(range(2, 10))
+#
+# Here we use the same training and validation months, as the testset contains only observations from one month.
+# For a real training, you should split the data into training and validation sets (e.g. training=months 2-10, validation=months 11-12).
+test_months = list(range(2, 12))
+train_months = list(range(2, 12))
 
 ########################################################################################
 # Initialize the datasets
@@ -71,75 +74,75 @@ print(config)
 #
 # The training is performed on patches of size `(128, 128)` for the SWAP data and `(256, 256)` for the AIA data. We use the `RandomPatchEditor` to extract random patches from the full images.
 
-#sdo_dataset = AIADataset(sdo_path, wavelength=171, months=train_months)
-#sdo_dataset = StorageDataset(sdo_dataset,
-#                             sdo_converted_path,
-#                             ext_editors=[RandomPatchEditor((256, 256))])
+sdo_dataset = AIADataset(sdo_path, wavelength=171, months=train_months)
+sdo_dataset = StorageDataset(sdo_dataset,
+                             sdo_converted_path,
+                             ext_editors=[RandomPatchEditor((256, 256))])
 
-#swap_dataset = SWAPDataset(swap_path, months=train_months)
-#swap_dataset = StorageDataset(swap_dataset, swap_converted_path,
-#                                ext_editors=[RandomPatchEditor((128, 128))])
+swap_dataset = SWAPDataset(swap_path, months=train_months)
+swap_dataset = StorageDataset(swap_dataset, swap_converted_path,
+                                ext_editors=[RandomPatchEditor((128, 128))])
 
-#sdo_valid = StorageDataset(AIADataset(sdo_path, wavelength=171, months=test_months, limit=100),
-#                           sdo_converted_path, ext_editors=[RandomPatchEditor((256, 256))])
-#swap_valid = StorageDataset(SWAPDataset(swap_path, months=test_months, limit=100),
-#                              swap_converted_path, ext_editors=[RandomPatchEditor((128, 128))])
+sdo_valid = StorageDataset(AIADataset(sdo_path, wavelength=171, months=test_months, limit=100),
+                           sdo_converted_path, ext_editors=[RandomPatchEditor((256, 256))])
+swap_valid = StorageDataset(SWAPDataset(swap_path, months=test_months, limit=100),
+                              swap_converted_path, ext_editors=[RandomPatchEditor((128, 128))])
 
 
 ########################################################################################
 # Initialize the data module
 
-#data_module = ITIDataModule(swap_dataset, sdo_dataset, swap_valid, sdo_valid, **config['data'])
+data_module = ITIDataModule(swap_dataset, sdo_dataset, swap_valid, sdo_valid, **config['data'])
 
 ########################################################################################
 # Define the plot settings for the callbacks
 
-#plot_settings_A = [
-#    {"cmap": cm.sdoaia171, "title": "SWAP 174", 'vmin': -1, 'vmax': 1},
-#]
-#plot_settings_B = [
-#    {"cmap": cm.sdoaia171, "title": "AIA 171", 'vmin': -1, 'vmax': 1},
-#]
+plot_settings_A = [
+    {"cmap": cm.sdoaia171, "title": "SWAP 174", 'vmin': -1, 'vmax': 1},
+]
+plot_settings_B = [
+    {"cmap": cm.sdoaia171, "title": "AIA 171", 'vmin': -1, 'vmax': 1},
+]
 
 ########################################################################################
 # Setup the logging for weights and biases (wandb)
-#logging_config = config['logging']
-#wandb_id = logging_config['wandb_id'] if 'wandb_id' in logging_config else None
-#log_model = logging_config['wandb_log_model'] if 'wandb_log_model' in logging_config else False
-#wandb_logger = WandbLogger(project=logging_config['wandb_project'], name=logging_config['wandb_name'], offline=False,
-#                           entity=logging_config['wandb_entity'], id=wandb_id, dir=config['base_dir'], log_model=log_model)
-#wandb_logger.experiment.config.update(config, allow_val_change=True)
+logging_config = config['logging']
+wandb_id = logging_config['wandb_id'] if 'wandb_id' in logging_config else None
+log_model = logging_config['wandb_log_model'] if 'wandb_log_model' in logging_config else False
+wandb_logger = WandbLogger(project=logging_config['wandb_project'], name=logging_config['wandb_name'], offline=False,
+                           entity=logging_config['wandb_entity'], id=wandb_id, dir=config['base_dir'], log_model=log_model)
+wandb_logger.experiment.config.update(config, allow_val_change=True)
 
 
 ########################################################################################
 # Start training
 
-#module = ITIModule(**config['model'])
+module = ITIModule(**config['model'])
 
 ########################################################################################
 # Setup save callbacks
-#checkpoint_callback = ModelCheckpoint(dirpath=base_dir, save_last=True, every_n_epochs=1, save_weights_only=False)
-#save_callback = SaveCallback(base_dir)
+checkpoint_callback = ModelCheckpoint(dirpath=base_dir, save_last=True, every_n_epochs=1, save_weights_only=False)
+save_callback = SaveCallback(base_dir)
 
 ########################################################################################
 # setup plot callbacks
 #
 # This will plot the predictions of the model on the validation data every epoch.
 
-#plot_callbacks = []
-#plot_callbacks += [PlotBAB(sdo_valid.sample(4), module, plot_settings_A=plot_settings_A, plot_settings_B=plot_settings_B)]
-#plot_callbacks += [PlotABA(swap_valid.sample(4), module, plot_settings_A=plot_settings_A, plot_settings_B=plot_settings_B)]
+plot_callbacks = []
+plot_callbacks += [PlotBAB(sdo_valid.sample(4), module, plot_settings_A=plot_settings_A, plot_settings_B=plot_settings_B)]
+plot_callbacks += [PlotABA(swap_valid.sample(4), module, plot_settings_A=plot_settings_A, plot_settings_B=plot_settings_B)]
 
 ########################################################################################
 # Start the training
 
-#n_gpus = torch.cuda.device_count()
-#trainer = Trainer(max_epochs=int(config['training']['epochs']),
-#                  logger=wandb_logger,
-#                  devices=n_gpus if n_gpus > 0 else None,
-#                  accelerator="gpu" if n_gpus >= 1 else None,
-#                  strategy='dp' if n_gpus > 1 else None,  # ddp breaks memory and wandb
-#                  num_sanity_val_steps=0,
-#                  callbacks=[checkpoint_callback, save_callback, *plot_callbacks],)
+n_gpus = torch.cuda.device_count()
+trainer = Trainer(max_epochs=int(config['training']['epochs']),
+                  logger=wandb_logger,
+                  devices=n_gpus if n_gpus > 0 else None,
+                  accelerator="gpu" if n_gpus >= 1 else None,
+                  strategy='dp' if n_gpus > 1 else None,  # ddp breaks memory and wandb
+                  num_sanity_val_steps=0,
+                  callbacks=[checkpoint_callback, save_callback, *plot_callbacks],)
 
-#trainer.fit(module, data_module, ckpt_path='last')
+trainer.fit(module, data_module, ckpt_path='last')
