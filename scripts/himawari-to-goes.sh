0ca304df1c11211d2a3f187c4d2464af6cb0b93e
annajungbluth <anna.jungbluth1993@gmail.com>
2025-10-06 21:22:14 +0000

    updated experiment script

--- scripts/himawari-to-goes.sh
+++ scripts/himawari-to-goes.sh
@@ -99,23 +99,26 @@ EPOCHS=100
 #         training.limit_val_batches=$LIMIT_VAL_BATCHES
 # done
 
-# 7-channel: All visible + near-IR channels (daytime only)
-declare -A variables_and_bands_7ch=(
-    ["7ch-0.47-3.9um"]="[470.0, 640.0, 870.0, 1380.0, 1610.0, 2250.0, 3890.0]"
+# 6-channel: All visible + near-IR channels (daytime only)
+declare -A variables_and_bands_6ch=(
+    ["6ch-0.47-3.9um"]="[470.0, 640.0, 870.0, 1380.0, 1610.0, 2250.0]"
 )
+
+A_bands="[470.0, 510.0, 640.0, 860.0, 1600.0,2300.0]"
+B_bands="[470.0, 640.0, 870.0, 1380.0, 1610.0, 2250.0]"
 
 FILTER_DAYTIME=True
 LIMIT_VAL_BATCHES=null # reset to full val set, since daytime filtering reduces val set size
 
-INPUT_DIM_A=7
-INPUT_DIM_B=7
+INPUT_DIM_A=6
+INPUT_DIM_B=6
 
-for variable in "${!variables_and_bands_7ch[@]}"; do
-    bands=${variables_and_bands_7ch[$variable]}
+for variable in "${!variables_and_bands_6ch[@]}"; do
+    bands=${variables_and_bands_6ch[$variable]}
     wandb_name="HIM8-GOES16-${variable}"
     python itipy/train/geo_to_geo.py \
-        data.A_bands="$bands" \
-        data.B_bands="$bands" \
+        data.A_bands="$A_bands" \
+        data.B_bands="$B_bands" \
         data.filter_daytime=$FILTER_DAYTIME \
         model.input_dim_a=$INPUT_DIM_A \
         model.input_dim_b=$INPUT_DIM_B \
@@ -123,6 +126,31 @@ for variable in "${!variables_and_bands_7ch[@]}"; do
         training.epochs=$EPOCHS \
         training.limit_val_batches=$LIMIT_VAL_BATCHES
 done
+
+# 7-channel: All visible + near-IR channels (daytime only)
+# declare -A variables_and_bands_7ch=(
+#     ["7ch-0.47-3.9um"]="[470.0, 640.0, 870.0, 1380.0, 1610.0, 2250.0, 3890.0]"
+# )
+
+# FILTER_DAYTIME=True
+# LIMIT_VAL_BATCHES=null # reset to full val set, since daytime filtering reduces val set size
+
+# INPUT_DIM_A=7
+# INPUT_DIM_B=7
+
+# for variable in "${!variables_and_bands_7ch[@]}"; do
+#     bands=${variables_and_bands_7ch[$variable]}
+#     wandb_name="HIM8-GOES16-${variable}"
+#     python itipy/train/geo_to_geo.py \
+#         data.A_bands="$bands" \
+#         data.B_bands="$bands" \
+#         data.filter_daytime=$FILTER_DAYTIME \
+#         model.input_dim_a=$INPUT_DIM_A \
+#         model.input_dim_b=$INPUT_DIM_B \
+#         logging.wandb_name="$wandb_name" \
+#         training.epochs=$EPOCHS \
+#         training.limit_val_batches=$LIMIT_VAL_BATCHES
+# done
 
 # 9-channel: All infrared channels
 # declare -A variables_and_bands_9ch=(
